f7e3d957a3a5fb890866bb78759dac67e96695a9
edwa1401 <edwa14011977@gmail.com>
2023-09-20 20:45:54 +0500

    complete tastks and done one test (as firsts view)

---
+++ books/tests.py
@@ -0,0 +1,40 @@
+from django.test.utils import setup_test_environment
+from django.test import Client
+from books.models import Book
+from django.test import TestCase
+
+
+
+client = Client()
+
+
+def create_book(
+        title,
+        author_full_name,
+        year_of_publishing,
+        copies_printed,
+        short_description
+        ):
+    return Book.objects.create(
+        title=title,
+        author_full_name=author_full_name,
+        year_of_publishing=year_of_publishing,
+        copies_printed=copies_printed,
+        short_description=short_description
+    )
+
+
+class QuestionIndexViewTests(TestCase):
+    def test__get_books__success(self):
+        book_1 = create_book(
+            title='any_book',
+            author_full_name='nobody',
+            year_of_publishing=2000,
+            copies_printed=2,
+            short_description='book about nothing'
+            )
+        response = self.client.get('/books/')
+        self.assertQuerysetEqual(
+            response.context['books'],
+            [book_1]
+        )
--- books/views.py
+++ books/views.py
@@ -1,3 +1,62 @@
+from django.core.exceptions import ObjectDoesNotExist
+from django.db.models.query import QuerySet
+from django.http import HttpRequest, HttpResponse, JsonResponse, HttpResponseNotFound
 from django.shortcuts import render
 
-# Create your views here.
+from books.models import Book
+
+
+def get_books() -> QuerySet | None:
+    books = Book.objects.all()
+    if not books:
+        return None
+    return books
+
+
+def books_view(request: HttpRequest) -> HttpResponse | None:
+    books = get_books()
+    return render(request, 'books.html', context={'books': books})
+
+
+def get_book_by_id(book_id: int) -> QuerySet | None:
+    try:
+        book = Book.objects.get(id=book_id)
+    except ObjectDoesNotExist:
+        return None
+    
+    if not book:
+        return None
+    return book
+    
+
+def single_book_view(request: HttpRequest, book_id: int) -> HttpResponse | None:
+    book = get_book_by_id(book_id)
+    return render(request, 'book.html', context={'book': book})
+
+
+def convert_book_from_query(book: QuerySet) -> dict:
+    return {
+        'id': book.pk,
+        'title': book.title,
+        'author_full_name': book.author_full_name,
+        'year_of_publishing': book.year_of_publishing,
+        'copies_printed': book.copies_printed,
+        'short_description': book.short_description
+    }
+
+def books_from_api_view(request: HttpRequest) -> list[JsonResponse] | None:
+    books = get_books()
+    if not books:
+        return HttpResponseNotFound('Нет книг')
+    view_books =[convert_book_from_query(book) for book in books]
+    return JsonResponse(view_books, safe=False)
+
+
+def single_book_from_api_view(request: HttpRequest, book_id: int) -> JsonResponse | None:
+    book = get_book_by_id(book_id)
+    if not book:
+        return HttpResponseNotFound('Нет книги с таким номером')
+    return JsonResponse(convert_book_from_query(book))
+
+
+    
